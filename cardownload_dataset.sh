0c70728c6204379e3ba54b2dc7b02b54c2ce5d3f
Jason <jasonmar53@hotmail.com>
2019-03-04 21:25:04 +0000

    added car download script

---
+++ cardownload_dataset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# https://github.com/junyanz/CycleGAN/blob/master/datasets/download_dataset.sh
+
+URL1=http://imagenet.stanford.edu/internal/car196/cars_train.tgz
+URL2=http://imagenet.stanford.edu/internal/car196/cars_test.tgz
+ZIP_FILE=./data/cars_train.tgz
+ZIP_FILE2=./data/cars_test.tgz
+TARGET_DIR=./data/stanford-cars-dataset/
+wget -N $URL1 -O $ZIP_FILE
+wget -N $URL2 -O $ZIP_FILE2
+mkdir -p $TARGET_DIR
+tar xvzf $ZIP_FILE -d ./data/
+tar xvzf $ZIP_FILE2 -d ./data/
